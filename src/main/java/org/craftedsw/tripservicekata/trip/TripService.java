830351d2c1e1f4d03cddf65e3aa41d83ca07ed16
Little Panda <>
2022-04-07 11:34:17 -0600

    Add all tests

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,7 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -21,12 +21,20 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
+				tripList = find_trip_by_user(user);
 			}
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
+	}
+
+	public User getLoggedUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+
+	public List<Trip> find_trip_by_user(User user) {
+		return TripDAO.findTripsByUser(user);
 	}
 	
 }
--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -5,12 +5,60 @@ import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.function.Executable;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 public class TripServiceTest {
 
+    private User loggedUser;
+    final private User GUEST = null;
+    final private User LOGGED_USER = new User();
+
     @Test void
     should_throw_an_exception_when_user_is_not_logged_in() {
-        TripService tripService = new TripService();
-        Assertions.assertThrows(UserNotLoggedInException.class, (Executable) tripService.getTripsByUser(null));
+        TestableTripService tripService = new TestableTripService();
+
+        loggedUser = GUEST;
+
+        Assertions.assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(loggedUser));
+    }
+
+    @Test void
+    should_return_empty_list_if_user_is_not_friend() {
+        TestableTripService tripService = new TestableTripService();
+
+        loggedUser = LOGGED_USER;
+
+        Assertions.assertEquals(tripService.getTripsByUser(loggedUser).size(), 0);
+    }
+
+    @Test void
+    should_return_trip_list_if_user_is_friend() {
+        TestableTripService tripService = new TestableTripService();
+
+        loggedUser = LOGGED_USER;
+        User friend = new User();
+        friend.addFriend(loggedUser);
+
+        friend.addTrip(new Trip());
+        friend.addTrip(new Trip());
+        friend.addTrip(new Trip());
+        friend.addTrip(new Trip());
+
+        Assertions.assertEquals(tripService.getTripsByUser(friend).size(), 4);
+
+    }
+
+    class TestableTripService extends TripService{
+        @Override
+        public User getLoggedUser() {
+            return loggedUser;
+        }
+
+        @Override
+        public List<Trip> find_trip_by_user(User user) {
+            return user.trips();
+        }
     }
 }
